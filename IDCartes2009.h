d39333e9b4abdaca52711203eb8eca67cca13bdf
lfuentes <lfuentes@b11ecc75-0911-4234-95f7-5f1a63128923>
2009-04-03 22:20:53 +0000

    [INFO] - ajout de la carte telemetres 2009 dans l'IE - ajout de la carte telemetres 2009 dans le simu - ajout des télémètres dans le simu - exposition de la carte telemetres 2009 à Python - choix définitif des IDs des cartes

---
+++ IDCartes2009.h
@@ -0,0 +1,23 @@
+// IDCartes2009.h - les IDs des cartes
+// En-tête utilisé par le simu et par l'IE
+
+enum IdCarte
+{
+	// ID Definitifs
+	ID_CARTE_CAN_USB = 0,
+	ID_CARTE_DEPLACEMENT = 1,
+	ID_CARTE_DETECT_ADV = 2,
+	ID_CARTE_ASSERVISSEMENT_ASCENCEUR = 3,
+	ID_CARTE_ASSERVISSEMENT_TAPIS = 4,
+	ID_CARTE_SERVONOVA_RAMASSAGE = 5,
+	ID_CARTE_SERVONOVA_TRI = 6,
+	ID_CARTE_TELEMETRE_EVITEMENT	= 7,
+	ID_CARTE_TELEMETRE_DETECT_PALETS	= 8,
+	ID_CARTE_COLORIMETRE = 9,
+	ID_CARTE_ALIM = 10,
+	ID_CARTE_CONTACTEURS = 11,	// tirette, couleur equipe, recalage ascenceur,
+								// recalage detect adv, recalage robot avant / arriere
+	ID_CARTE_CONTACTEURS_RESERVE = 12, // Au cas ou
+	ID_CARTE_BALISE_XBEE = 13,
+	ID_CARTE_DEBUG       = 17
+};
