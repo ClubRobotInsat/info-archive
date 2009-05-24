ecd728660032fe6db0bb45c3c92fc8c996f29ca2
lfuentes <lfuentes@b11ecc75-0911-4234-95f7-5f1a63128923>
2009-05-24 11:54:40 +0000

    [INFO] - TheMonster, et des modifs un peu partout...c'est le dernier commit de la coupe, fait le lendemain, donc je ne me rappelle plus tout ce qui avait été fait ^^

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@
 # initialise une connexion avec l'epia
 
 ROBOT_ADR="robot@10.30.42.42"
-IA_PATH="./robot/2009/IA/strategie_exemple"
+IA_PATH="./robot/2009/IA/the_monster"
 PARAM_PATH="./robot/2009/IA/params_webcam_rouge.txt ./robot/2009/IA/params_webcam_vert.txt ./robot/2009/IA/parametres_reconnaissance.txt"
 ROBOT_PATH="$ROBOT_ADR:~"
 
 # copie de l'ia
-gnome-terminal -e scp $IA_PATH $ROBOT_PATH
+gnome-terminal -e "scp $IA_PATH $ROBOT_PATH"
 
 # copie des reglages
 gnome-terminal -e scp $PARAM_PATH $ROBOT_PATH
