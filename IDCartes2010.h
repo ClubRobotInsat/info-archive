2dee30ab3fbd123c7974d7cd8e7ec13f45ad0a00
lczornom <lczornom@b11ecc75-0911-4234-95f7-5f1a63128923>
2009-10-13 14:58:22 +0000

    [Info] Grosse update générale de l'info (IA + controle wiimote + libRobot2010)

---
+++ IDCartes2010.h
@@ -0,0 +1,15 @@
+// IDCartes2010.h - les IDs des cartes
+// En-tête utilisé par le simu et par l'IE
+
+enum IdCarte
+{
+	// ID Definitifs
+	ID_CARTE_CAN_USB = 0,
+	ID_CARTE_DEPLACEMENT = 1,
+	ID_CARTE_CONTACTEURS = 11,	// tirette, couleur equipe, recalage ascenceur,
+								// recalage detect adv, recalage robot avant / arriere
+	ID_BROADCAST = 15,
+	ID_CARTE_DEBUG_DESSIN = 16,
+	ID_CARTE_DEBUG = 17,
+	ID_CARTE_LCD = 18
+};
