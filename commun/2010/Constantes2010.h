3c13d5e05d39210043229c224cbd96b68d56cd55
jgraulle <jgraulle@b11ecc75-0911-4234-95f7-5f1a63128923>
2010-06-06 19:09:19 +0000

    r26 | lancelot | 2010-05-13 03:08:01 +0200 (jeu. 13 mai 2010) | 3 lignes
    [info]
    *reglage fourche position
    *modif IA par rapport aux tests

--- commun/2010/Constantes2010.h
+++ commun/2010/Constantes2010.h
@@ -94,11 +94,11 @@ static const int positionPinceGauche[POSITION_PINCE_NBR] = {700, 1050, 1240, 142
 static const int positionPinceDroite[POSITION_PINCE_NBR] = {2380, 2100, 2050, 1750, 1575};		// ouvert, lache objet, bloque tomate, bloque mais, ferme
 
 static const int positionAscenseur[POSITION_ASCENSEUR_NBR] = {-10000, -8500, -5000, -1900, -300, 0}; // bas, pos arrachage, haut (position bascule)), hauteur en mm
-static const int positionFourcheGauche[POSITION_FOURCHE_NBR] = {900, 1930, 1970, 2020, 2170, 2280}; //tres_bas, bas, milieu, milieu_plus, haut, tres_haut
-static const int positionFourcheDroite[POSITION_FOURCHE_NBR] = {2180, 1250, 1184, 1116, 1000, 880}; //tres_bas, bas, milieu, milieu_plus, haut, tres_haut
+static const int positionFourcheGauche[POSITION_FOURCHE_NBR] = {930, 1210, 1800, 1930, 2080, 2400}; //tres_bas, bas, milieu, milieu_plus, haut, tres_haut
+static const int positionFourcheDroite[POSITION_FOURCHE_NBR] = {2050, 1780, 1180, 1040, 900, 700}; //tres_bas, bas, milieu, milieu_plus, haut, tres_haut
 static const int positionPorte[POSITION_PORTE_NBR] = {600, 1940}; 				// ouvert, ferme
 static const int positionLoquet[POSITION_LOQUET_NBR] = {1390, 720}; 				// ouvert, ferme
-static const int positionPlancher[POSITION_PLANCHER_NBR] = {1550, 2100, 2380}; 		// haut = releve, milieu, bas = abaisse
+static const int positionPlancher[POSITION_PLANCHER_NBR] = {1550, 2075, 2380}; 		// haut = releve, milieu, bas = abaisse
 
 static const int deltaPositionServo = 20; // > resolution sur les valeurs de position des servos
 static const int deltaPositionMoteur = 5; // > resolution sur les valeurs de position des moteurs (TODO : mettre la bonne valeur)
