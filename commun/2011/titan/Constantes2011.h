38b7258ceb0d0dd39e4be1fd38bec9d01948bf51
lancelot <lancelot@b11ecc75-0911-4234-95f7-5f1a63128923>
2010-11-04 23:12:13 +0000

    [INFO] Ajout de la libRobot2011 et de la CarteDebug2011 côté IE.

---
+++ commun/2011/titan/Constantes2011.h
@@ -0,0 +1,40 @@
+// Constantes2011.h
+
+#ifndef CONSTANTES_2011_H
+#define CONSTANTES_2011_H
+
+#include <iostream>
+
+enum CouleurRobot {COULEUR_ROBOT_ROUGE = 0, COULEUR_ROBOT_BLEU = 1};
+
+// Pour permettre l'affichage d'une Couleur :
+inline std::ostream& operator<<(std::ostream& os, const CouleurRobot& couleur)
+{
+	switch(couleur)
+	{
+	case COULEUR_ROBOT_ROUGE:
+		os << "rouge";
+		break;
+	case COULEUR_ROBOT_BLEU:
+		os << "bleu";
+		break;
+	}
+
+	return os;
+}
+
+enum Bras
+{
+	BRAS_GAUCHE = 0,
+	BRAS_DROIT = 1
+};
+
+// Numeros des contacteurs et les roles associes
+enum NumContacteur
+{
+	CONTACTEUR_TIRETTE = 0,
+	CONTACTEUR_CHOIX_EQUIPE = 1
+};
+
+#endif //CONSTANTES_2011_H
+
---
+++ commun/2011/titan/IDCartes2011.h
@@ -0,0 +1,12 @@
+// IDCartes2011.h - les IDs des cartes
+// En-tête utilisé par le simu et par l'IE
+
+enum IdCarte
+{
+	// ID Definitifs
+	ID_CARTE_CAN_USB = 0,
+	ID_CARTE_DEPLACEMENT = 1,
+	ID_CARTE_CONTACTEURS = 11,
+	ID_CARTE_DEBUG_DESSIN = 16,
+	ID_CARTE_DEBUG = 17
+};
