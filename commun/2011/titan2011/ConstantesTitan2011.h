00f055f8e17e8b2bc36f72b9d83f6d4204170363
lancelot <lancelot@b11ecc75-0911-4234-95f7-5f1a63128923>
2011-05-29 00:24:30 +0000

    [info] IaCheckmateV2Beta
    Juste avant de scanner, on sort le bras.
    On rentre le bras et sort le bras (marche pas à tous les coups) pour le 1er pion de la ligne
    On clipse le 2e
    On dépose dans la zone protégée
    On va construire la tour avec 3 pieces
    On va déposer
    On va construire la tour avec la tete restante, et le pion restant de la zone verte
    On va déposer
    
    On fait une promenade sur la table avec les bras ouvert.
    Pour l'instant on va déposer les pions dans les 2 zones bonus
    Mais c'est pour l'instant crade !!!!!!!!!!!!!!!!!!!!!!

--- commun/2011/titan2011/ConstantesTitan2011.h
+++ commun/2011/titan2011/ConstantesTitan2011.h
@@ -47,7 +47,7 @@ enum ServosNovaAscenseur{
 // Numeros des positions des servos et moteur asservi et les roles associes
 enum PositionGriffeNum {POSITION_GRIFFE_OUVERTE = 0, POSITION_GRIFFE_BLOCAGE_PION, POSITION_GRIFFE_FERMEE, POSITION_GRIFFE_NBR};
 enum PositionPinceNum {POSITION_PINCE_OUVERTE = 0, POSITION_PINCE_BLOCAGE_PION, POSITION_PINCE_FERMEE, POSITION_PINCE_NBR};
-enum PositionBrasNum {POSITION_BRAS_OUVERT = 0, POSITION_BRAS_FERME, POSITION_BRAS_NBR};
+enum PositionBrasNum {POSITION_BRAS_OUVERT = 0, POSITION_BRAS_FERME, POSITION_BRAS_OUVERT_MAX, POSITION_BRAS_NBR};
 
 enum PositionAscenseurNum {POSITION_ASCENSEUR_BAS = 0, POSITION_ASCENSEUR_POUR_CLIPSER, POSITION_ASCENSEUR_POUR_NE_PAS_CLIPSER, POSITION_ASCENSEUR_NBR};
 
@@ -59,8 +59,8 @@ static const int positionGriffeDroite[POSITION_GRIFFE_NBR] = {1550, 1240, 700};
 static const int positionPinceGauche[POSITION_PINCE_NBR] = {700, 1700, 1800};
 static const int positionPinceDroite[POSITION_PINCE_NBR] = {2350, 1240, 1200};
 
-static const int positionBrasGauche[POSITION_BRAS_NBR]= {20000, 0};
-static const int positionBrasDroit[POSITION_BRAS_NBR]={-20000, 0};
+static const int positionBrasGauche[POSITION_BRAS_NBR]= {20000, 0, 25500};
+static const int positionBrasDroit[POSITION_BRAS_NBR]={-20000, 0, -25500};
 
 static const int positionAscenseur[POSITION_ASCENSEUR_NBR]=  {0, 700000, 870000};
 //simu ancienne valeur : maintenant ça marche à peu près sans changer les valeurs {0, 10000, 20000};
