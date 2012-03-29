b91e601659b8058f39bc4840dc9cebbbd300b699
Jérôme Sivadier <jerome.sivadier@gmail.com>
2012-03-29 15:17:47 +0000

    New constantes, new cartes servos
    Former-commit-id: 962efcedaf0e4b7c19f529015cde00a5682268b6

--- commun/2012/Constantes2012.h
+++ commun/2012/Constantes2012.h
@@ -37,39 +37,38 @@ namespace Constantes2012
 
 	///////////////////////////////////// Numéro des servos /////////////////////////////////////////////
 	// Indice des différents servo de ramassage dans le tableau de positions
-	enum ServosPieceTotem {
+	enum ServosAvantDroit {
 		SERVO_HAUT_DROIT 					= 0,
-		SERVO_HAUT_GAUCHE					= 1,
+		SERVO_PINCE_TOTEM_DROIT				= 1,
 		SERVO_BAS_DROIT						= 2,
-		SERVO_BAS_GAUCHE					= 3,
-		SERVO_PIECE_TOTEM_NBR				= 4
+		SERVO_BRAS_CALE_DROIT				= 3,
+		SERVO_AD_NBR						= 4
 	};
 
-	// Position des servos PieceTotem
-	enum PositionServosPieceTotem {POSITION_SERVO_TOTEM_OUVERT = 0, POSITION_SERVO_TOTEM_MOITIE, POSITION_SERVO_TOTEM_FERME, POSITION_SERVO_TOTEM_NBR};
-	static const int positionServosHautGauche[POSITION_SERVO_TOTEM_NBR] = {0,200,500}; //TBD
-	static const int positionServosHautDroit[POSITION_SERVO_TOTEM_NBR] = {0,200,500}; //TBD
-	static const int positionServosBasGauche[POSITION_SERVO_TOTEM_NBR] = {0,200,500}; //TBD
-	static const int positionServosBasDroit[POSITION_SERVO_TOTEM_NBR] = {0,200,500}; //TBD
-
+	// Position des servos Avant Droit
+	enum PositionServosAvantDroit {POSITION_SERVO_AD_OUVERT = 0, POSITION_SERVO_AD_MOITIE, POSITION_SERVO_AD_FERME, POSITION_SERVO_AD_NBR};
+	static const int positionServosHautDroit[POSITION_SERVO_AD_NBR] = {0,200,500}; //TBD
+	static const int positionsServosPinceTotemDroit[POSITION_SERVO_AD_NBR] = {0,200,1000}; // TBD
+	static const int positionServosBasDroit[POSITION_SERVO_AD_NBR] = {0,200,500}; //TBD
+	static const int positionServosCaleDroit[POSITION_SERVO_AD_NBR] = {0,200,500}; //TBD
 
 	///
 	// Indice des différents servo de dépose dans le tab de positions
-	enum ServosCoffreCale {
-		SERVO_BRAS_CALE_DROITE				= 0,
-		SERVO_BRAS_CALE_GAUCHE				= 1,
-		SERVO_COFFRE						= 2,
-		SERVO_COFFRE_AVANT					= 3,
-		SERVO_COFFRE_CALE_NBR				= 4
+	enum ServosAvantGauche {
+		SERVO_HAUT_GAUCHE 					= 0,
+		SERVO_PINCE_TOTEM_GAUCHE			= 1,
+		SERVO_BAS_GAUCHE					= 2,
+		SERVO_BRAS_CALE_GAUCHE				= 3,
+		SERVO_AG_NBR						= 4
 	};
 
-	// Position des servos CoffreCale
-	enum PositionServosCale {POSITION_SERVO_CALE_OUVERT = 0, POSITION_SERVO_CALE_MOITIE, POSITION_SERVO_CALE_FERME, POSITION_SERVO_CALE_NBR};
-	static const int positionServosCaleGauche[POSITION_SERVO_CALE_NBR] = {0,200,500}; //TBD
-	static const int positionServosCaleDroit[POSITION_SERVO_CALE_NBR] = {0,200,500}; //TBD
-	static const int positionsServoCoffreAvant[POSITION_SERVO_CALE_NBR] = {0,200,500}; // TBD
-	enum PositionServoCoffre {POSITION_SERVO_COFFRE_OUVERT = 0, POSITION_SERVO_COFFRE_FERME, POSITION_SERVO_COFFRE_NBR};
-	static const int positionServoCoffre[POSITION_SERVO_COFFRE_NBR] = {0,1000}; //TBD
+	// Position des servos Avant Gauche
+	enum PositionServosAvantGauche {POSITION_SERVO_AG_OUVERT = 0, POSITION_SERVO_AG_MOITIE, POSITION_SERVO_AG_FERME, POSITION_SERVO_AG_NBR};
+	static const int positionServosHautGauche[POSITION_SERVO_AG_NBR] = {0,200,500}; //TBD
+	static const int positionsServosPinceTotemGauche[POSITION_SERVO_AG_NBR] = {0,200,1000}; // TBD
+	static const int positionServosBasGauche[POSITION_SERVO_AG_NBR] = {0,200,500}; //TBD
+	static const int positionServosCaleGauche[POSITION_SERVO_AG_NBR] = {0,200,500}; //TBD
+
 
 	///
 	// Indice des différents servo de ramassage lingot et carte dans le tab de positions
@@ -84,7 +83,6 @@ namespace Constantes2012
 	// Position des servos CarteEtLingotTotem
 	enum PositionServosCarteEtLingotTotem {POSITION_SERVO_CARTE_LINGOT_OUVERT = 0, POSITION_SERVO_CARTE_LINGOT_MILIEU, POSITION_SERVO_CARTE_LINGOT_FERME, POSITION_SERVO_CARTE_LINGOT_NBR};
 	static const int positionsServosTotemGauche[POSITION_SERVO_CARTE_LINGOT_NBR] = {0,200,1000}; // TBD
-	static const int positionsServosTotemDroit[POSITION_SERVO_CARTE_LINGOT_NBR] = {0,200,1000}; // TBD
 	static const int positionsServosPinceCarte[POSITION_SERVO_CARTE_LINGOT_NBR] = {0,200,1000};
 	static const int positionsServosBrasCarte[POSITION_SERVO_CARTE_LINGOT_NBR] = {0,200,1000};
 
@@ -94,8 +92,8 @@ namespace Constantes2012
 	static const int tableauPositionsLongitudinalesGlissiereCoffre[NBR_POSITIONS_LONGITUDINALES_COFFRE] = {0,20000}; // TDB
 
 	// Positions possibles pour la moissoneuse (moteur)
-	enum PositionsMoissoneuse {MOISSONEUSE_ARRET_VERTICAL = 0, MOISSONEUSE_ARRET_HORIZONTAL, NBR_POSITIONS_MOISSONEUSE};
-	static const int tableauPositionsMoissoneuse[NBR_POSITIONS_MOISSONEUSE] = {0,20000}; // TDB
+	//enum PositionsMoissoneuse {MOISSONEUSE_ARRET_VERTICAL = 0, MOISSONEUSE_ARRET_HORIZONTAL, NBR_POSITIONS_MOISSONEUSE};
+	//static const int tableauPositionsMoissoneuse[NBR_POSITIONS_MOISSONEUSE] = {0,20000}; // TDB
 
 	// Position longitudinale la glissière pour attraper les lingots (moteur)
 	enum PositionsLongitudinalesGlissiereLingot	{GLISSIERE_RANGEE = 0, GLISSIERE_SORTIE, GLISSIERE_MI_RANGEE, NBR_POSITIONS_LONGITUDINALES_GLISSIERE};
@@ -103,7 +101,7 @@ namespace Constantes2012
 
   	static const int deltaPositionServo = 20; // > resolution sur les valeurs de position des servos
   	static const int deltaPositionMoteur = 70; // > resolution sur les valeurs de position des moteurs (TODO : mettre la bonne valeur)
-  	static const int vitesseRotationMoissoneuse = 4000; // > Vitesse de rotation de la moissoneuse (TODO : TDB !)
+  	//static const int vitesseRotationMoissoneuse = 4000; // > Vitesse de rotation de la moissoneuse (TODO : TDB !)
 }
 
 #endif //CONSTANTES_2012_H
