b4b2bde4a6e19f8d74855233970e8a7fc7a3968a
Nicolas Seydoux <nicolas.seydoux@aliceadsl.fr>
2013-05-05 20:15:23 +0000

    [info] Nouvelles ID des cartes de Pan
    Former-commit-id: e987f87216c5e31115324bddd3538566f710f34a

--- commun/2013/pan2013/Constantes2013.h
+++ commun/2013/pan2013/Constantes2013.h
@@ -25,8 +25,8 @@ namespace ConstantesPan2013
     
     //ID des servos
 	enum Servos {
-		SERVO_BRAS_DROIT            = 2,
-		SERVO_BRAS_GAUCHE           = 1,
+		SERVO_BRAS_DROIT            = 1,
+		SERVO_BRAS_GAUCHE           = 2,
 		SERVO_RAMASSE               = 0,
 		SERVO_EJECTE                = 3,
         SERVO_NBR                   = 4
@@ -40,6 +40,7 @@ namespace ConstantesPan2013
         SERVO_RANGE,
         SERVO_BRAS_NBR_POSITIONS
     };
+
     //Index des positions dans le tableau
     enum PositionServosRamasse {
         SERVO_BAS = 0,
@@ -47,18 +48,11 @@ namespace ConstantesPan2013
         SERVO_RELEVE,
         SERVO_RAMASSE_NBR_POSITIONS
     };
-    //Index des positions dans le tableau
-    enum PositionServosEjecte {
-        SERVO_OK = 0,
-        SERVO_POURRIE,
-        SERVO_EJECTE_NBR_POSITIONS
-    };
     
     //Position des servos
-    static const int positionsServosBrasDroit[SERVO_BRAS_NBR_POSITIONS] =   {2400,1350,800, 500};
-    static const int positionsServosBrasGauche[SERVO_BRAS_NBR_POSITIONS] =  {2200,1950,1000, 500};
+    static const int positionsServosBrasGauche[SERVO_BRAS_NBR_POSITIONS] =   {1600,1420,1200, 600};
+    static const int positionsServosBrasDroit[SERVO_BRAS_NBR_POSITIONS] =  {1050,1255,1455, 2100};
     static const int positionServosRamasse[SERVO_RAMASSE_NBR_POSITIONS] =   {600,2000, 1000};
-    static const int positionServosEjecte[SERVO_EJECTE_NBR_POSITIONS] =     {1500,1100};
 
     
     /***********************************/
--- commun/2013/pan2013/IDCartes2013.h
+++ commun/2013/pan2013/IDCartes2013.h
@@ -10,10 +10,12 @@ namespace IDCartes2013
 	{
         ID_CARTE_CAN_USB                    = 0,
         ID_CARTE_DEPLACEMENT                = 1,
-        ID_CARTE_ASSERVISSEMENT_LANCEUR     = 2,
         ID_CARTE_CONTACTEURS                = 3,
         ID_CARTE_SERVOS                     = 4,
-        ID_CARTE_EVITEMENT                  = 5,
+        ID_CARTE_EVITEMENT                  = 8,
+        ID_CARTE_DEBUG_DESSIN				= 6,
+        ID_CARTE_IHM						= 7,
+        ID_CARTE_POMPE						= 9
 	};
 }
 #endif
