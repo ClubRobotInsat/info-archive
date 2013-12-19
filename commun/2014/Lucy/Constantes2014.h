5e23ddaf8f0779e2fff726097c23c77f00ff0e74
Rémi Saurel <r4.saurel@gmail.com>
2013-12-19 16:06:00 +0000

    [info] namespace, fichiers inutiles supprimés
    Former-commit-id: 8eff0f220900ef1e5913d561edb6c09e13aa86ab

--- commun/2014/Lucy/Constantes2014.h
+++ commun/2014/Lucy/Constantes2014.h
@@ -1,7 +1,31 @@
 // Constantes2013.h
 
-#ifndef CONSTANTES_2013_H
-#define CONSTANTES_2013_H
+#ifndef CONSTANTES_LUCY_2014_H
+#define CONSTANTES_LUCY_2014_H
+
+#ifdef TARGET_SIMULATEUR
+
+#include "../../../simulateur/simulateur/Robot/Carte.h"
+#include "../../../simulateur/cartes/CarteDeplacement2009.h"
+#include "../../../simulateur/cartes/CarteContacteurs2007.h"
+#include "../../../simulateur/cartes/CarteIHM.h"
+#include "../../../simulateur/cartes/CarteServosNova2009.h"
+#include "../../../simulateur/cartes/CarteDebugDessin.h"
+#include "../../../simulateur/cartes/CartePneumatique2013.h"
+#include "../../../simulateur/cartes/CarteDetectAdv2009.h"
+
+#else
+
+#include "../../../robot/Robot/Carte.h"
+#include "../../../robot/Cartes/CarteDeplacement2009.h"
+#include "../../../robot/Cartes/CarteContacteurs2007.h"
+#include "../../../robot/Cartes/CarteIHM.h"
+#include "../../../robot/Cartes/CarteServosNova2009.h"
+#include "../../../robot/Cartes/CarteDebugDessin.h"
+#include "../../../robot/Cartes/CartePneumatique.h"
+#include "../../../robot/Cartes/CarteDetectAdv2009.h"
+
+#endif
 
 namespace ConstantesLucy2014 {
 	
@@ -70,6 +94,58 @@ namespace ConstantesLucy2014 {
 	//Vitesse de rotation du moteur du lanceur
   	extern const int vitessesRotationLanceur[LANCEUR_NBR_VITESSES];
 	
+	namespace IDCartesLucy2014 {
+		enum {
+			ID_CARTE_CAN_USB                    = 0,
+			ID_CARTE_DEPLACEMENT                = 1,
+			ID_CARTE_CONTACTEURS                = 3,
+			ID_CARTE_SERVOS                     = 4,
+			ID_CARTE_EVITEMENT                  = 8,
+			ID_CARTE_DEBUG_DESSIN				= 6,
+			ID_CARTE_IHM						= 7,
+			ID_CARTE_POMPE						= 9
+		};
+	}
+	using namespace IDCartesLucy2014;
+
+	template<int ID_CARTE>
+	struct CarteLucy {
+		
+	};
+	
+	/*template<>
+	 struct CarteToumai<ID_CARTE_CAN_USB> {
+	 typedef int type;
+	 };*/
+	template<>
+	struct CarteLucy<ID_CARTE_DEPLACEMENT> {
+		typedef CarteDeplacement2009 type;
+		static constexpr char const *name = "Carte déplacement";
+	};
+	template<>
+	struct CarteLucy<ID_CARTE_EVITEMENT> {
+		typedef CarteDetectAdv2009 type;
+	};
+	template<>
+	struct CarteLucy<ID_CARTE_IHM> {
+		typedef CarteIHM type;
+	};
+	template<>
+	struct CarteLucy<ID_CARTE_CONTACTEURS> {
+		typedef CarteContacteurs2007 type;
+	};
+	template<>
+	struct CarteLucy<ID_CARTE_POMPE> {
+#ifdef TARGET_SIMULATEUR
+		typedef CartePneumatique2013 CartePneumatique;
+#endif
+		typedef CartePneumatique type;
+	};
+	template<>
+	struct CarteLucy<ID_CARTE_DEBUG_DESSIN> {
+		typedef CarteDebugDessin type;
+	};
+
 }
 
 #endif //CONSTANTES_2013_H
