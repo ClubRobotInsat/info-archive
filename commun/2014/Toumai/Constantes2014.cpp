12b98fbd56114c121cb4357051ce2dcdf48795a0
Jérôme Sivadier <jerome.sivadier@gmail.com>
2014-05-17 22:15:53 +0000

    [Info] Commit des stratégies + quelques debugs sur les mutex
    Former-commit-id: 7f607e5998e3c2e3bc0810d2a00d90513e5c27b1

--- commun/2014/Toumai/Constantes2014.cpp
+++ commun/2014/Toumai/Constantes2014.cpp
@@ -10,12 +10,12 @@ namespace ConstantesToumai2014 {
 
 	const int positionPinceSaisie[POSITION_PINCE_S_NBR] 		= {1300, /*1770*/1790};
 	const int positionPinceRetournement[POSITION_PINCE_R_NBR] 	= {600, 2490};
-	const int positionTVentouse[POSITION_T_VENTOUSE_NBR] 		= {800, 1430/*1460*/, 2350};
-	const int positionSerpeBas[POSITION_SERPE_BAS_NBR] 			= {1950, 1730, 600};
+	const int positionTVentouse[POSITION_T_VENTOUSE_NBR] 		= {800, 1430/*1460*/, /*2350*/2310};
+	const int positionSerpeBas[POSITION_SERPE_BAS_NBR] 			= {1950, 1730, /*600*/580};
 	const int positionVideFeu[POSITION_VIDE_FEU_NBR] 			= {1990, 1340};
 
 	// Moteurs
 	const int tableauPositionsAsc[NBR_POSITIONS_ASC] =
-			{13889, 19078, 24661, 30671, 24000, 17000, 2000, 0};
+			{/*13889*/14200, /*19078*/22000, /*24661*/27000, 30671, 24000, 17000, 2000, 0};
 }
 
