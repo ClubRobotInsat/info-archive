376c34141d426d3ed9c6b7d0482b746ed63eaf9a
Nicolas Seydoux <nicolas.seydoux@aliceadsl.fr>
2014-05-18 00:26:35 +0000

    [Info] Encore quelques améliorations
    Former-commit-id: 5a3d6025451223ca2c77a47d0a2f497f35bb27a0

--- commun/2014/Toumai/Constantes2014.cpp
+++ commun/2014/Toumai/Constantes2014.cpp
@@ -9,13 +9,13 @@ namespace ConstantesToumai2014 {
 	const int positionPanierFruit[POSITION_PANIER_NBR] 			= {1570, 2400, 1350, 1700};
 
 	const int positionPinceSaisie[POSITION_PINCE_S_NBR] 		= {1300, /*1770*/1790};
-	const int positionPinceRetournement[POSITION_PINCE_R_NBR] 	= {600, 2490};
-	const int positionTVentouse[POSITION_T_VENTOUSE_NBR] 		= {800, 1430/*1460*/, /*2350*/2310};
+	const int positionPinceRetournement[POSITION_PINCE_R_NBR] 	= {/*600*/575, 2490};
+	const int positionTVentouse[POSITION_T_VENTOUSE_NBR] 		= {/*800*/780, 1430/*1460*/, /*2350*/2310};
 	const int positionSerpeBas[POSITION_SERPE_BAS_NBR] 			= {1950, 1730, /*600*/580};
 	const int positionVideFeu[POSITION_VIDE_FEU_NBR] 			= {1990, 1340};
 
 	// Moteurs
 	const int tableauPositionsAsc[NBR_POSITIONS_ASC] =
-			{/*13889*/14200, /*19078*/22000, /*24661*/27000, 30671, 24000, 17000, 2000, 0};
+			{/*13889*/14200, /*19078*/21500, /*24661*/27000, 30671, /*24000*/21500, 17000, 2000, 0};
 }
 
