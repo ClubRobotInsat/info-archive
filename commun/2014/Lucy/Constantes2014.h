3bdebc88c9078786390fe8d1e1950f6e464a25b6
Nicolas Seydoux <nicolas.seydoux@aliceadsl.fr>
2014-05-22 22:47:24 +0000

    Retour à un algo plus stable pour le CAN
    Former-commit-id: ebe808e046616791f68698cdad5eadcb21d52639

--- commun/2014/Lucy/Constantes2014.h
+++ commun/2014/Lucy/Constantes2014.h
@@ -84,7 +84,7 @@ namespace ConstantesLucy2014 {
 			//ID_CARTE_CONTACTEURS 		= 3,
 			ID_CARTE_SERVOS 			= 4,
 			ID_CARTE_EVITEMENT 			= 8,
-			//ID_CARTE_DEBUG_DESSIN 		= 3,
+			ID_CARTE_DEBUG_DESSIN 		= 5,
 			//ID_CARTE_IHM 				= 7,
 			//ID_CARTE_POMPE 			= 9,
 			ID_CARTE_IO					= 3
