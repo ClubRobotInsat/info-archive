0cdb36a5109e7c9e0431f4a75dde29f567d8224e
Rémi Saurel <r4.saurel@gmail.com>
2014-06-16 07:18:48 +0000

    [info] Message valable pour les 2 commits précédents en plus de celui-ci (oups). Ajout d’une rotation automatique au chargement des modèles COLLADA pour se placer dans le même référentiel que Blender. Nom personnalisé pour chaque thread important, plus facile à débugguer. Divers ajustements.
    Former-commit-id: e8e15b990034bca2d99de59b58f0aae880187f8a

--- commun/Commun.h
+++ commun/Commun.h
@@ -3,12 +3,42 @@
 
 // sleep() et msleep() : routines d'attente en secondes et en millisecondes
 #ifdef WIN32
+
 #include <windows.h>
+
 #define sleep(t) Sleep((t)*1000)
 #define msleep(t) Sleep(t)
-#else
+
+#else // defined(WIN32)
+
 #include <unistd.h>
 #define msleep(t) usleep((t)*1000)
-#endif
+
+#endif // defined(WIN32)
+
+#include <memory>
+#include <type_traits>
+
+namespace std {
+	// Pallie à l'absence de std::make_unique dans C++11, à venir dans C++14 cependant.
+	// Source : http://stackoverflow.com/a/13512344/1276072
+
+	template <typename T, typename... Args>
+	unique_ptr<T> make_unique_helper(false_type, Args&&... args) {
+		return unique_ptr<T>(new T(forward<Args>(args)...));
+	}
+
+	template <typename T, typename... Args>
+	unique_ptr<T> make_unique_helper(true_type, Args&&... args) {
+		static_assert(extent<T>::value == 0, "make_unique<T[N]>() is forbidden, please use make_unique<T[]>().");
+
+		return unique_ptr<T>(new typename remove_extent<T>::type[sizeof...(Args)]{forward<Args>(args)...});
+	}
+
+	template <typename T, typename... Args>
+	unique_ptr<T> make_unique(Args&&... args) {
+		return make_unique_helper<T>(is_array<T>(), forward<Args>(args)...);
+	}
+}
 
 #endif
