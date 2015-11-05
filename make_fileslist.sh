0a5466708a0612319a90160914af7d1cd42abc0d
Rémi Saurel <r4.saurel@gmail.com>
2015-11-05 10:56:25 +0100

    Suppression de pthreads-win32, de libfreenect et de vieil UML
    Former-commit-id: 19ff66f5fbae20ba361b8fa8ffc3ee1a8a86c481

--- make_fileslist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILESLIST=fileslist.txt
-find . -name "*.cpp" | grep -v "./tags" > $FILESLIST
-find . -name "*.c" | grep -v "./tags" >> $FILESLIST
-find . -name "*.hpp" | grep -v "./tags" >> $FILESLIST
-find . -name "*.h" | grep -v "./tags" >> $FILESLIST
-find . -name "SConstruct" | grep -v "./tags" >> $FILESLIST
