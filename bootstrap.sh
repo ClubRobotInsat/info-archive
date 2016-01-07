6d9694a28768697202b29b2489830f37e8a52d65
Rémi Saurel <r4.saurel@gmail.com>
2016-01-07 19:03:30 +0100

    Màj bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+oldpwd="$OLDPWD"
+
+cd "$dir"
+
 git submodule update --init
 petri/bootstrap.sh
-petri/Editor/build.sh
+#petri/Editor/build.sh
+xbuild petri/Editor/Projects/Petri.csproj
+mkdir -p robot/Principal/petri/src
+
+cd "$oldpwd"
