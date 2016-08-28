384639459e55c12e413ba3e6ade1f57a1aea6421
Paul Florence <florencepaul@hotmail.fr>
2016-08-28 15:26:50 +0200

    fix pour la connection en serial au can physique.

--- elec/Interface_Can/CanListener.cpp
+++ elec/Interface_Can/CanListener.cpp
@@ -13,7 +13,7 @@ CanListener::CanListener(std::string& port) : _shallStopListening(false), _refre
         std::cout << "CAN listening on : " << port.substr(7,port.length()-12) << ":1234" <<std::endl;
     }
     else {
-        _can.reset(new Commun::CAN(std::make_unique<Commun::RS232>(port)));
+        _can.reset(new Commun::CAN(std::make_unique<Commun::RS232>("/dev/" + port)));
     }
 
     _can->setTemporisation(10_ms);
