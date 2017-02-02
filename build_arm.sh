d9a73a86dc301aa55c1bd8634f117628377502dd
gbip <florencepaul@hotmail.fr>
2017-02-02 14:40:42 +0100

    Script pour lancer l'IA. Script pour crosscompiler l'IA.

---
+++ build_arm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cores=4
+
+dgreen='\e[1;32m'
+egreen='\e[0m' 
+
+if !( [[ "$1" =~ ^[-+]?[0-9]+$ ]] )
+then
+	echo -e "$dgreen Compiling with -j 4 $egreen"
+else
+	cores=$1
+	echo -e "$dgreen Compiling with -j $cores $egreen"
+fi
+
+if `! test -d build_arm`  
+	then	mkdir build_arm
+fi
+
+cd build_arm
+echo -e "$dgreen Generating CMakeFile $egreen"
+cmake .. -DBITS=64 -DCMAKE_TOOLCHAIN=Cross-Compilation.cmake 
+
+echo -e "$dgreen Building IAPrincipal $egreen"
+make IAPrincipal -j $cores
+
+echo -e "$dgreen Building IAWiimote $egreen"
+make IAWiimote -j $cores
