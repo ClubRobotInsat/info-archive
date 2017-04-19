49098ef14ca9199d7eed6f6adc1ef1f609fe7da7
Paul Florence <florencepaul@hotmail.fr>
2017-04-19 20:54:46 +0200

    Meilleur script de lancement du simu.

--- simu_launcher.sh
+++ simu_launcher.sh
@@ -10,22 +10,25 @@ then
 elif ! [ "$1" = "blue" ]
 then
 	echo "Usage : simu_launcher.sh [blue|yellow]"
+	exit 1
 fi
 
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "Launching server..."
-"$dir"/websimulator/serve.sh 2> /dev/null
+echo "Launching web_server..."
+../websimulator/serve.sh &
 
 echo "Launching Petri..."
 "$dir"/petri/petri &
+
+echo salut
 
 if `test -e "$dir"/build/Simu2017`
 then
 	echo "Launching simu..."
 	"$dir"/build/Simu2017 --robot on --world on --color "$color"
-elif `test -e "$dir"/cmake-build-debug/Simu2017`
+elif `test -e "$dir"/cmake-build-debug/simulateur/Simu2017`
 then
 	echo "Launching simu..."
-	"$dir"/cmake-build-debug/Simu2017 --robot on --world on --color "$color"
+	"$dir"/cmake-build-debug/simulateur/Simu2017 --robot on --world on --color "$color"
 fi
