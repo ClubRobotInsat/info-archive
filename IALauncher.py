f8fe7b0e84e1659261ec3537dd69ddd5391da609
Paul Florence <florencepaul@hotmail.fr>
2017-05-04 18:11:07 +0200

    Amélioration du script de lancement de l'IA.

--- IALauncher.py
+++ IALauncher.py
@@ -17,53 +17,90 @@ env['LD_LIBRARY_PATH']="/home/pi/Desktop"
 def test() :
     print("ohoh")
 
+
+
+# Return a list of all acceptable USB connection
+def scan_usb_connection() :
+    files = os.listdir("/dev")
+    result = list()
+    for file in files :
+        if file[0:6] == "ttyUSB" :
+            result.append("/dev/"+file)
+            print(file)
+        elif file[0:6] == "ttyACM":
+            result.append("/dev/"+file)
+            print(file)
+    return result
+
+
+scan_usb_connection()
+
 class app :
     def __init__(self) :
         
-        #Tk top level 
+        # ---- Tk top level 
         self.root = tkinter.Tk()
         self.root.resizable(width=False,height=False)
         self.root.wm_title(string="Lanceur d'IA Club Robot INSA Toulouse")
         self.root.wm_minsize(800,480)
         self.root.attributes('-fullscreen', True)
         
-        #Canvas
+        # ---- Canvas
         self.c_term=tkinter.Frame(self.root)
         self.c_term.place(relx=0.25,rely=0,relwidth=0.75,relheight=1)
 
-        #Terminal
+        # ---- Terminal
         os.system('xterm -into %d -geometry 97x37 -sb -e tail -f tmp&' % self.c_term.winfo_id())
         
-        #Color internal variable
+        # ---- Color internal variable
         self.color = "blue"
 
-        #IA Processus
+        # ---- IA Processus
         self.ia="" 
 
-        #IA Picker
+        # ---- IA Picker
         self.ia_picker_option = ('IAWiimote','IAPrincipal')
         self.ia_map = {'IAWiimote' : 'sudo ./IAWiimote RS232 /dev/ttyUSB0', 'IAPrincipal' : 'sudo ./IAPrincipal RS232'}
         self.ia_picker_value = tkinter.StringVar()
         self.ia_picker_value.set(self.ia_picker_option[0])
-        self.ia_picker = ttk.Combobox(self.root,textvariable=self.ia_picker_value,values=self.ia_picker_option,state='readonly',justify='center',height=30)
-        self.ia_picker.place(relx=0,rely=0.5,relwidth=0.25,relheight=0.16667)
 
-        #Run
+        # Dropdown menu text style
+        # text_font = ('Arial', '15')
+        # self.root.option_add('*TCombobox*Listbox.justify','center')
+        # self.root.option_add('*TCombobox*Listbox.font', text_font)
+
+        #self.ia_picker = ttk.Combobox(self.root,textvariable=self.ia_picker_value,values=self.ia_picker_option,state='readonly',justify='center',height=30)
+        self.ia_picker = tkinter.OptionMenu(self.root,self.ia_picker_value,*self.ia_picker_option)
+        
+        # Get the menu because OptionMenu is a shitty widget
+        menu = self.ia_picker.nametowidget(self.ia_picker.menuname) 
+        menu.configure(font=('Arial', 70)) 
+        
+        self.ia_picker.place(relx=0,rely=0.5,relwidth=0.25,relheight=0.16667)
+        self.available_connection=list()
+        # ---- Refresh Connection
+        self.b_refresh = tkinter.Button(self.root,text="Refresh Connections",command=self.callback_refresh,state="normal")
+
+        # ---- Run
         self.b_run = tkinter.Button(self.root,text="GO !",command=self.callback_run,state="normal")
         self.b_run.place(relx=0,rely=0.83333,relwidth=0.25,relheight=0.16667)
-        #Kill
+        # ---- Kill
         self.b_kill=tkinter.Button(self.root,text="KILL IA",state="disabled",command=self.callback_kill)
         self.b_kill.place(relx=0,rely=0.6667,relwidth=0.25,relheight=0.16667)
 
-        #Color 
+        # ---- Color 
         self.b_color=tkinter.Button(self.root,text="Bleu",bg="blue",activebackground="blue",fg="black",command=self.callback_color_change)
         #self.b_color.config(activebackground=self.get_color())
         self.b_color.place(relx=0,rely=0,relwidth=0.25,relheight=0.25)
 
 
-        # Kill Launcher
+        # ---- Kill Launcher
         self.b_kill_launcher=tkinter.Button(self.root,text="Kill Interface",command=self.callback_kill_interface)
         self.b_kill_launcher.place(relx=0,rely=0.25,relwidth=0.25,relheight=0.25)
+
+    def callback_refresh(self):
+        self.available_connection=scan_usb_connection
+
 
     def callback_kill_interface(self):
         sys.exit(0)
