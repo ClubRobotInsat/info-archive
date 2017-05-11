3fbbd83bb6f7575d0b3f4a337d0a4cb819f0ed4b
panipanipa <denis@gouvine.fr>
2017-05-11 16:38:39 +0200

    le debut des deplacements

--- commun/RobotPrincipal/Constantes.h
+++ commun/RobotPrincipal/Constantes.h
@@ -69,7 +69,7 @@ namespace ConstantesPrincipal {
 	const repere::Repere REFERENCE_YELLOW =
 	    repere::Repere({3_m, 0_m}, repere::Multiplicateur::SENS_NEGATIF, repere::Multiplicateur::SENS_POSITIF);
 
-	const Vector3m START_ROBOT_POSITION(902_mm, 182_cm, 15_cm);
+	const Vector3m START_ROBOT_POSITION(1.05_m-29.4_cm/2, 2_m-29.4_cm/2-2.1_cm, 15_cm);
 	const Angle START_ROBOT_ANGLE(-90_deg);
 
 	/////////////////// CONTACTEURS ///////////////////
