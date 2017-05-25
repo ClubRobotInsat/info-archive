c3a25a7dbe526498001f9b0baea780599fe45616
Paul Florence <florencepaul@hotmail.fr>
2017-05-25 19:58:31 +0200

    Meilleur API pour sauvegarder des trames de preset.

--- elec/Interface_Can/Preset.cpp
+++ elec/Interface_Can/Preset.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Preset.h"
+#include <iostream>
 
 
 Preset::Preset(uint8_t id, uint8_t cmd, uint8_t* data)
@@ -19,10 +20,32 @@ std::string Preset::toString() {
 	return std::__cxx11::string();
 }
 
+Preset::Preset(std::string rawPreset) {}
 
-PresetArray::PresetArray(std::string path) {}
 
+PresetArray::PresetArray(std::string path) {
+	Json::Value json;
+	std::cin >> json;
+
+	const Json::Value array = json["presets"];
+	for(int i = 0; i < array.size(); ++i) {
+		_preset.push_back(Preset(array[i].asString()));
+	}
+}
+
+
+void PresetArray::saveToFile(std::string path) {
+
+	Json::Value json;
+	for(Preset preset : _preset) {
+		json["presets"] =
+	}
+}
+
+void PresetArray::addPreset(Preset preset) {
+	_preset.push_back(preset);
+}
 
 std::vector<Preset> PresetArray::getArray() {
-	return std::vector<Preset>();
+	return _preset;
 }
