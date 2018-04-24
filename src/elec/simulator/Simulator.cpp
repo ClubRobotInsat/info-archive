ad3d26cca6eec64cb58f74797c5aba65c9de3689
Paul Florence <florencepaul@hotmail.fr>
2018-04-24 18:59:11 +0200

    MAJ submodule.

--- src/elec/simulator/Simulator.cpp
+++ src/elec/simulator/Simulator.cpp
@@ -2,14 +2,22 @@
 // Created by paul on 24/04/18.
 //
 
+#include <iostream>
 #include <thread>
 
 int main_elec();
-int main_robot();
+int main_robot(int argc, char** argv);
+
+int main_robot_2() {
+	return main_robot(0, nullptr);
+}
 
 int main() {
+	std::cout << "Début de l'elec" << std::endl;
 	std::thread thread_elec(&main_elec);
-	std::thread thread_robot(&main_robot);
+	std::cout << "Début de l'info " << std::endl;
+	std::thread thread_robot(&main_robot_2);
 	thread_elec.join();
 	thread_robot.join();
+	return 0;
 }
