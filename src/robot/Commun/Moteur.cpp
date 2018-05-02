fd5659506fd40c92f69159a2214d5157b2176053
panipanipa <denis@gouvine.fr>
2018-05-02 19:11:26 +0200

    ajout des fonctions positionnerMoteursBloquants

--- src/robot/Commun/Moteur.cpp
+++ src/robot/Commun/Moteur.cpp
@@ -5,24 +5,27 @@
 #include "Moteur.h"
 
 ResultatAction Commun::MoteurManager::positionnerMoteurBloquant(uint8_t moteur, Angle pos) {
+	/*std::vector<uint8_t > vec_moteur;
+	vec_moteur.insert(moteur);*/
+	return this->positionnerMoteursBloquants({moteur}, pos);
+	/*
+	    // TODO cf avec Paul Meis
+	    auto TIMEOUT_MOTEUR = 1_s;
+	    auto timeoutDate = TimePoint::now() + TIMEOUT_MOTEUR;
 
-	// TODO cf avec Paul Meis
-	auto TIMEOUT_MOTEUR = 1_s;
-	auto timeoutDate = TimePoint::now() + TIMEOUT_MOTEUR;
+	    _carte.positionAngle(moteur, pos);
 
-	_carte.positionAngle(moteur, pos);
+	    while(true) {
+	        if(TimePoint::now() >= timeoutDate) {
+	            logWarn("TIMEOUT !");
+	            return ResultatAction::TIMEOUT;
+	        }
 
-	while(true) {
-		if(TimePoint::now() >= timeoutDate) {
-			logWarn("TIMEOUT !");
-			return ResultatAction::TIMEOUT;
-		}
-
-		if(_carte.lirePosition(moteur) == true) {
-			return ResultatAction::REUSSI;
-		}
-		sleep(50_ms);
-	}
+	        if(_carte.lirePosition(moteur) == true) {
+	            return ResultatAction::REUSSI;
+	        }
+	        sleep(50_ms);
+	    }*/
 }
 
 ResultatAction Commun::MoteurManager::positionnerMoteurBloquant(uint8_t moteur, std::size_t nbr_tours, CarteMoteurs2018::SensRotation sens) {
@@ -60,4 +63,61 @@ ResultatAction Commun::MoteurManager::tournerOff(uint8_t moteur) {
 ResultatAction Commun::MoteurManager::tournerOnBrushless(uint8_t moteur) {
 	_carte.activerBrushless(moteur);
 	return ResultatAction::REUSSI;
+}
+
+ResultatAction Commun::MoteurManager::positionnerMoteursBloquants(std::vector<uint8_t> moteurs, Angle pos) {
+
+	auto TIMEOUT_MOTEUR = 1_s;
+	auto timeoutDate = TimePoint::now() + TIMEOUT_MOTEUR;
+
+	for(uint8_t motor : moteurs) {
+		_carte.positionAngle(motor, pos);
+	}
+
+	do {
+		if(TimePoint::now() >= timeoutDate) {
+			logWarn("TIMEOUT !");
+			return ResultatAction::TIMEOUT;
+		}
+
+		bool fini = true;
+		for(uint8_t motor : moteurs) {
+			fini = fini && _carte.lirePosition(motor);
+		}
+
+		if(fini) {
+			return ResultatAction::REUSSI;
+		}
+		sleep(50_ms);
+	} while(true);
+}
+
+ResultatAction Commun::MoteurManager::positionnerMoteursBloquants(std::vector<uint8_t> moteurs,
+                                                                  std::size_t nbr_tours,
+                                                                  CarteMoteurs2018::SensRotation sens) {
+
+	auto TIMEOUT_MOTEUR = 1_s * nbr_tours;
+	auto timeoutDate = TimePoint::now() + TIMEOUT_MOTEUR;
+
+
+	for(uint8_t motor : moteurs) {
+		_carte.positionTour(motor, static_cast<uint8_t>(nbr_tours), sens);
+	}
+
+	do {
+		if(TimePoint::now() >= timeoutDate) {
+			logWarn("TIMEOUT !");
+			return ResultatAction::TIMEOUT;
+		}
+
+		bool fini = true;
+		for(uint8_t motor : moteurs) {
+			fini = fini && _carte.lirePosition(motor);
+		}
+
+		if(fini) {
+			return ResultatAction::REUSSI;
+		}
+		sleep(50_ms);
+	} while(true);
 }
--- src/robot/Commun/Moteur.h
+++ src/robot/Commun/Moteur.h
@@ -7,7 +7,7 @@
 
 #include "../Cartes/CarteMoteurs2018.h"
 #include "Robot.h"
-
+#include <vector>
 
 // @Denis : Manager à écrire (exemple depuis 'Servo.h'), après avoir écrit la CarteMoteur2018.h
 namespace Commun {
@@ -16,19 +16,25 @@ namespace Commun {
 		// TODO
 		MoteurManager(CarteMoteurs2018& carteMoteur) : _carte(carteMoteur){};
 
-		// fonctions pour moteurs asservis
-		ResultatAction positionnerMoteurBloquant(uint8_t moteur, Angle pos);
-		ResultatAction positionnerMoteurBloquant(uint8_t moteur, std::size_t nbr_tours, CarteMoteurs2018::SensRotation);
+		// fonctions pour activer plusieurs moteurs
+		ResultatAction positionnerMoteursBloquants(std::vector<uint8_t> moteurs, Angle pos);
+		ResultatAction positionnerMoteursBloquants(std::vector<uint8_t> moteurs, std::size_t nbr_tours, CarteMoteurs2018::SensRotation sens) {
 
-		// fonctions pour moteurs non asservis
-		ResultatAction tournerOn(uint8_t moteur, CarteMoteurs2018::SensRotation);
-		ResultatAction tournerOff(uint8_t moteur);
 
-		ResultatAction tournerOnBrushless(uint8_t moteur);
+			// fonctions pour moteurs asservis
+			ResultatAction positionnerMoteurBloquant(uint8_t moteur, Angle pos);
+			ResultatAction positionnerMoteurBloquant(uint8_t moteur, std::size_t nbr_tours, CarteMoteurs2018::SensRotation);
 
-	private:
-		CarteMoteurs2018& _carte;
-	};
+			// fonctions pour moteurs non asservis
+			ResultatAction tournerOn(uint8_t moteur, CarteMoteurs2018::SensRotation);
+			ResultatAction tournerOff(uint8_t moteur);
+
+			ResultatAction tournerOnBrushless(uint8_t moteur);
+
+
+		private:
+			CarteMoteurs2018& _carte;
+		};
 }
 
 #endif // ROOT_MOTEUR_H
