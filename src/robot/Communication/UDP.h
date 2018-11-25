7c0e4d9a5efe68d5aa55a0dc46d9cf14f604dc1f
Terae <bigey@etud.insa-toulouse.fr>
2018-11-25 21:25:30 +0100

    Fix warnings for noreturn functions.

--- src/robot/Communication/UDP.h
+++ src/robot/Communication/UDP.h
@@ -10,6 +10,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wsuggest-attribute=noreturn"
 #include <asio.hpp>
 #pragma GCC diagnostic pop
 
