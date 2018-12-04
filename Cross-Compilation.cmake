a4802c7f0393c6110023a87067d0ce003ae7eace
Terae <bigey@etud.insa-toulouse.fr>
2018-12-04 13:20:12 +0100

    Update of cross-compilation scripts.

--- Cross-Compilation.cmake
+++ Cross-Compilation.cmake
@@ -4,16 +4,25 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-#SET(CMAKE_C_COMPILER /opt/cros/x-tools/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-gcc )
-SET(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc-5)
+if(EXISTS "/usr/bin/arm-linux-gnueabihf-gcc-8")
+    set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc-8)
+elseif(EXISTS "/usr/bin/arm-linux-gnueabihf-gcc-7")
+    set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc-7)
+else()
+    message(FATAL_ERROR "No supported version of ARM GCC cross compiler found.")
+endif()
 
-#SET(CMAKE_CXX_COMPILER /opt/cros/x-tools/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-g++ )
-SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-5)
+if(EXISTS "/usr/bin/arm-linux-gnueabihf-g++-8")
+    set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-8)
+elseif(EXISTS "/usr/bin/arm-linux-gnueabihf-g++-7")
+    set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-7)
+else()
+    message(FATAL_ERROR "No supported version of ARM G++ cross compiler found.")
+endif()
 
 # where is the target environment
 #SET(CMAKE_FIND_ROOT_PATH /opt/cros/x-tools/arm-unknown-linux-gnueabi)
 SET(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
-
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
