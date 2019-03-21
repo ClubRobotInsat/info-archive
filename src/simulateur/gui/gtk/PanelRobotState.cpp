767474f8ec783653bf4a379fa3b2a79845b14667
KimiNako <katran@etud.insa-toulouse.fr>
2019-03-21 14:30:46 +0100

    Add test

--- src/simulateur/gui/gtk/PanelRobotState.cpp
+++ src/simulateur/gui/gtk/PanelRobotState.cpp
@@ -1,5 +1,25 @@
+#include <resources/EmbeddedFiles.h>
 #include "PanelRobotState.h"
 
 PanelRobotState::PanelRobotState() {
-	set_size_request(-1, 400);
+    _builder = Gtk::Builder::create_from_string(EmbeddedFiles::readText("glade/state-panel.glade"));
+}
+
+
+
+void refreshPanel(JSON json){
+
+}
+
+JSON PanelRobotState::generateTestJSON() {
+    JSON json;
+    json["servo1"] = JSON();
+    json["servo2"] = JSON();
+
+    json["servo1"]["blocked"] = true;
+    json["servo2"]["blocked"] = false;
+    json["servo1"]["mode"] = "Speed";
+    json["servo1"]["color"] = "Color";
+
+    return json;
 }
--- src/simulateur/gui/gtk/PanelRobotState.h
+++ src/simulateur/gui/gtk/PanelRobotState.h
@@ -1,13 +1,17 @@
 #ifndef ROOT_PANELROBOTSTATE_H
 #define ROOT_PANELROBOTSTATE_H
 
+#include <Json.h>
 #include "GtkInclude.h"
 
 class PanelRobotState : public Gtk::Frame {
 public:
 	PanelRobotState();
 
+    void refreshPanel(JSON json);
+    JSON generateTestJSON();
 private:
+    Glib::RefPtr<Gtk::Builder> _builder;
 };
 
 
