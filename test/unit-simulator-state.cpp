96a8e257e456c240931350909cd6dc800f261104
KimiNako <katran@etud.insa-toulouse.fr>
2019-03-28 15:53:45 +0100

    Test on robot simulator state

---
+++ test/unit-simulator-state.cpp
@@ -0,0 +1,17 @@
+//
+// Created by katran on 21/03/19.
+//
+
+#include "catch.hpp"
+
+#include "../src/simulateur/gui/gtk/PanelRobotState.h"
+
+TEST_CASE("Global constants", "[integration]") {
+	SECTION("GENERATE JSON") {
+		PanelRobotState panel = new PanelRobotState();
+		JSON j = panel.generateTestJSON();
+
+		CHECK(j.)
+	}
+	SECTION("TEST PRINT JSON") {}
+}
