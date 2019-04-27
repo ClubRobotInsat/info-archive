319e26224f85c3dcd1fca8ea2cd6808ce104cd1b
KimiNako <katran@etud.insa-toulouse.fr>
2019-04-27 17:14:06 +0200

    Add panel state and its functions

--- src/simulateur/gui/gtk/PanelRobotState.cpp
+++ src/simulateur/gui/gtk/PanelRobotState.cpp
@@ -1,38 +1,104 @@
 #include "PanelRobotState.h"
 #include <resources/EmbeddedFiles.h>
 
-// template <class UnaryFunction>
 PanelRobotState::PanelRobotState() {
-	// _builder = Gtk::Builder::create_from_string(EmbeddedFiles::readText("glade/state-panel.glade"));
-}
+	_builder = Gtk::Builder::create_from_string(EmbeddedFiles::readText("glade/state-panel.glade"));
+	_builder->get_widget("quit-button", _quitButton);
+	_builder->get_widget("tree-view", _treeView);
 
+	// We set columns
+	refTreeModel = Gtk::TreeStore::create(m_columns);
+	_treeView->set_model(refTreeModel);
+
+	// signals
+	_quitButton->signal_clicked().connect(sigc::mem_fun(*this, &PanelRobotState::on_button_quit));
+	_treeView->signal_row_activated().connect(sigc::mem_fun(*this, &PanelRobotState::on_treeview_row_activated));
+	show_all_children();
+}
 
 void PanelRobotState::refreshPanel(JSON json) {
-	setElement(json, [](JSON::const_iterator it) {
-		std::cout << *it << std::endl; // Print in console
-	});
+	setTreeView(json);
 }
 
-template <class UnaryFunction>
-void PanelRobotState::setElement(JSON json, UnaryFunction f) {
-	for(auto it = json.begin(); it != json.end(); ++it) {
-		if(it->is_structured()) {
-			// setElement(it, f);
-		} else {
-			f(it);
+void PanelRobotState::setTreeView(JSON& root) {
+	if(!root.empty()) {
+		for(auto it = root.begin(); it != root.end(); ++it) {
+
+			// We create father row
+			Gtk::TreeModel::Row row = *(refTreeModel->append());
+			row[m_columns.col_name] = it.key();
+
+			// We add his children
+			addChild(*it, row);
+		}
+	}
+	_treeView->append_column("Name", m_columns.col_name);
+}
+
+
+void PanelRobotState::addChild(JSON& root, Gtk::TreeModel::Row& row) {
+	if(!root.empty()) {
+		for(auto it = root.begin(); it != root.end(); ++it) {
+
+			// We create child row
+			Gtk::TreeModel::Row childrow = *(refTreeModel->append(row.children()));
+			childrow[m_columns.col_name] = it.key();
+			childrow[m_columns.col_status] = it.value();
+
+			// If we got great children, we add them
+			addChild(*it, childrow);
 		}
 	}
 }
 
-JSON PanelRobotState::generateTestJSON() {
-	JSON json;
-	json["servo1"] = JSON();
-	json["servo2"] = JSON();
-
-	json["servo1"]["blocked"] = true;
-	json["servo2"]["blocked"] = false;
-	json["servo1"]["mode"] = "Speed";
-	json["servo1"]["color"] = "Color";
-
-	return json;
+// We quit the panel when we click on Quit button
+void PanelRobotState::on_button_quit() {
+	hide();
 }
+
+// We veil the row's details when clicking it
+void PanelRobotState::on_treeview_row_activated(const Gtk::TreeModel::Path& path, Gtk::TreeViewColumn* column) {
+	Gtk::TreeModel::iterator iter = refTreeModel->get_iter(path);
+	if(iter) {
+		Gtk::TreeModel::Row row = *iter;
+		std::cout << "Row activated: Name = " << row[m_columns.col_name] << std::endl;
+	}
+}
+
+
+// void PanelRobotState::refreshPanel(JSON json) {
+//	setElement(json, [](JSON::const_iterator it) {
+//		std::cout << *it << std::endl; // Print in console
+//	});
+//}
+//
+// template <class UnaryFunction>
+// void PanelRobotState::setElement(JSON json, UnaryFunction f) {
+//	for(auto it = json.begin(); it != json.end(); ++it) {
+//		if(it->is_structured()) {
+//			PanelRobotState::setElement(*it, f);
+//		} else {
+//			f(it);
+//		}
+//	}
+//}
+
+
+// JSON PanelRobotState::generateTestJSON() {
+//	JSON json;
+//	json["servo1"] = JSON();
+//	json["servo2"] = JSON();
+//
+//	json["servo1"]["blocked"] = true;
+//	json["servo2"]["blocked"] = false;
+//	json["servo1"]["mode"] = "Speed";
+//	json["servo1"]["color"] = "Color";
+//
+//	return json.dump();
+//}
+
+// int main(){
+//     std::cout << "test" << std::endl;
+//     JSON json = PanelRobotState::generateTestJSON();
+//     PanelRobotState
+// }
--- src/simulateur/gui/gtk/PanelRobotState.h
+++ src/simulateur/gui/gtk/PanelRobotState.h
@@ -8,13 +8,46 @@
 class PanelRobotState : public Gtk::Frame {
 public:
 	PanelRobotState();
+
 	void refreshPanel(JSON json);
-	JSON generateTestJSON();
+
+protected:
+	// Signal handler
+	void on_button_quit();
+	void on_treeview_row_activated(const Gtk::TreeModel::Path& path, Gtk::TreeViewColumn* column);
+
+	// Tree model columns
+	class ModelColumns : public Gtk::TreeModel::ColumnRecord {
+	public:
+		ModelColumns() {
+			add(col_name);
+			add(col_details);
+			add(col_status);
+		}
+
+		Gtk::TreeModelColumn<Glib::ustring> col_name;
+		Gtk::TreeModelColumn<Glib::ustring> col_details;
+		Gtk::TreeModelColumn<Glib::ustring> col_status;
+	};
+
+	ModelColumns m_columns;
+
+	// Child widgets:
+	Gtk::Box box;
+	Glib::RefPtr<Gtk::TreeStore> refTreeModel;
 
 private:
-	template <class UnaryFunction>
-	void setElement(JSON json, UnaryFunction f);
+	Glib::RefPtr<Gtk::Builder> _builder;
+
+	Gtk::Button* _quitButton;
+	Gtk::TreeView* _treeView;
+
+	void setTreeView(JSON& root);
+	void addChild(JSON& root, Gtk::TreeModel::Row& row);
+	//	void setElement(JSON json, UnaryFunction f);
 };
 
 
+// pour tester on fait simulator [ debug
+// https://developer.gnome.org/gtkmm-tutorial/stable/sec-treeview-examples.html.en
 #endif // ROOT_PANELROBOTSTATE_H
