bdab5f9f8cca508b20e1d7838c4dea5e94a81be4
AbdelSaTd <tarnagda@etud.insa-toulouse.fr>
2020-03-21 19:37:57 +0100

    operator x namespace bug on compilation fixed

--- src/robot/Strategy/Environment/node.cpp
+++ src/robot/Strategy/Environment/node.cpp
@@ -1,23 +1,26 @@
 ﻿#include "node.h"
 #include <unordered_map>
 
-RobotEnv::Node::Node(int x_, int y_, float v_, float effectiveCost_, float heuristicCost_) {
-	x = x_;
-	y = y_;
-	staticValue = v_;
-	effectiveCost = effectiveCost_;
-	heuristicCost = heuristicCost_;
-	dynamicValue = 0;
-}
+namespace RobotEnv {
+	Node::Node(int x_, int y_, float v_, float effectiveCost_, float heuristicCost_) {
+		x = x_;
+		y = y_;
+		staticValue = v_;
+		effectiveCost = effectiveCost_;
+		heuristicCost = heuristicCost_;
+		dynamicValue = 0;
+	}
 
-float RobotEnv::Node::totalCost() const {
-	return heuristicCost;
-}
+	float Node::totalCost() const {
+		return heuristicCost;
+	}
 
-bool operator==(RobotEnv::Node const& node1, RobotEnv::Node const& node2) {
-	return node1.x == node2.x && node1.y == node2.y;
-}
+	bool operator==(Node const& node1, Node const& node2) {
+		return node1.x == node2.x && node1.y == node2.y;
+	}
 
-bool operator<(RobotEnv::Node const& node1, RobotEnv::Node const& node2) {
-	return node1.totalCost() < node2.totalCost();
-}
+	bool operator<(Node const& node1, Node const& node2) {
+		return node1.totalCost() < node2.totalCost();
+	}
+
+} // namespace RobotEnv
